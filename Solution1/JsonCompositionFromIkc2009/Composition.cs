90c614359c40ccade35d1f5d1dd32df78bef30f7
Bertus van Dalen <dalenek@delagelanden.com>
2018-05-27 23:49:47 +0200

    simplified the basic design, will add entity specifics later

--- Solution1/JsonCompositionFromIkc2009/Composition.cs
+++ Solution1/JsonCompositionFromIkc2009/Composition.cs
@@ -9,56 +9,48 @@ namespace JsonCompositionFromIkc2009.TableStorage
 {
     public class Composition : TableEntity
     {
-        public JsonCompositionFromIkc2009.Composition Item { get; set; }
+        public string Environment { get; set; }
+        public string Scroll { get; set; }
+        public string Config { get; set; }
         public Composition(
-            JsonCompositionFromIkc2009.Composition composition, 
+            string composition_identifier, 
             string environment_identifier, 
             string scroll_identifier, 
             string config_identifier)
         {
-            this.PartitionKey = composition.id.ToString();
-            this.RowKey = composition.id.ToString();
-            this.Item = composition;
+            this.PartitionKey = composition_identifier;
+            this.RowKey = composition_identifier;
+            this.Environment = environment_identifier;
+            this.Scroll = scroll_identifier;
+            this.Config = config_identifier;
         }
     }
 
     public class Environment : TableEntity
     {
-        public string EnvironmentIdentifier { get; set; }
-        public JsonCompositionFromIkc2009.Environment Item { get; set; }
-        public Environment(JsonCompositionFromIkc2009.Environment environment, string environment_identifier)
+        public Environment(string environment_identifier)
         {
             this.PartitionKey = environment_identifier;
             this.RowKey = environment_identifier;
-            this.Item = environment;
-            this.EnvironmentIdentifier = environment_identifier;
         }
     }
 
     public class Scroll : TableEntity
     {
-        public string ScrollIdentifier { get; set; }
-        public JsonCompositionFromIkc2009.Scrollitem Item { get; set; }
         public Scroll(JsonCompositionFromIkc2009.Scrollitem scroll, string scroll_identifier)
         {
             this.PartitionKey = scroll_identifier;
             this.RowKey = scroll_identifier;
-            this.Item = scroll;
-            this.ScrollIdentifier = scroll_identifier;
         }
 
     }
 
     public class Config : TableEntity
     {
-        public string ConfigIdentifier { get; set; }
-        public Dictionary<string, string> Item { get; set; }
         public Config(string config_identifier, Dictionary<string, string> config)
         {
             this.PartitionKey = config_identifier;
             this.RowKey = config_identifier;
-            this.ConfigIdentifier = config_identifier;
-            this.Item = config;
         }
     }
 }
